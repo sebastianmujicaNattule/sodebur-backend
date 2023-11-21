6bb4de518c21c1724c32eaaa51f8ced67440b894
sebastianmujica@gmail.com <sebastianmujica@gmail.com>
2023-11-21 11:05:50 +0100

    config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'sodebur2023!',
+    user: 'sodebur',
     password: 'sodebur2023!',
     database:'sodebur'
 });
