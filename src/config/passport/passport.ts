619d27cf5769770b5ecff56bc1998dcff3859d78
sebastianmujica@gmail.com <sebastianmujica@gmail.com>
2024-01-02 12:17:01 +0100

    server configuration

--- src/config/passport/passport.ts
+++ src/config/passport/passport.ts
@@ -2,18 +2,21 @@ import dotenv from 'dotenv';
 import { PassportStatic } from 'passport';
 import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 import { User } from '../../domain/models/user';
-import { FindByIdUser } from '../../domain/use-cases/user/find-by-id'
+import { FindByIdUser } from '../../domain/use-cases/user/find-by-id';
+import 
 
-export default (passport: PassportStatic) => {
+export default (passport: PassportStatic, db) => {
   dotenv.config();
   passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.SECRET_KEY || 'jvns',
   },
   (payload, done) => {
+
     const findByIdUseCase= new FindByIdUser(payload)  
     const user =  findByIdUseCase.execute(payload)
-      if (user) {
+    
+    if (user) {
         return done(null, user);
       }else{
         return done({ error: 'No encontrado' }, false);
